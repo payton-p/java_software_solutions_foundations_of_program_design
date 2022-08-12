182f08a535de1ad3835270cf0c4aeff703895f07
Payton Pietron <paytonpietron@gmail.com>
2022-08-11 23:02:12 -0400

    Apply formatter

--- chapter_2_interactive_programs/src/scanner_examples/ScannerExample.java
+++ chapter_2_interactive_programs/src/scanner_examples/ScannerExample.java
@@ -3,16 +3,17 @@ package scanner_examples;
 import java.util.Scanner;
 
 public class ScannerExample {
-	public static void main(String[] args) {
-		// Takes input from the keyboard - System.in represents a standard input stream, which be default is the keyboard 
-		Scanner scan = new Scanner(System.in); 
-		
-		System.out.println("Enter a line of text:"); 
-		
-		String message = scan.nextLine(); 
-		
-		System.out.println("You entered: \n" + message); 
-		
-		scan.close(); 
-	}
+  public static void main(String[] args) {
+    // Takes input from the keyboard - System.in represents a standard input stream, which be
+    // default is the keyboard
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Enter a line of text:");
+
+    String message = scan.nextLine();
+
+    System.out.println("You entered: \n" + message);
+
+    scan.close();
+  }
 }

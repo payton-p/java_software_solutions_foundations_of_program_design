aa8447e45c8e5e369bc9abda21d44f2a5a4f44fe
Payton Pietron <paytonpietron@gmail.com>
2022-08-25 00:44:05 -0400

    Simple class example

---
+++ chapter4_writing_classes/src/chapter4_writing_classes/Movie.java
@@ -0,0 +1,61 @@
+package chapter4_writing_classes;
+
+public class Movie {
+  // Keep encapsulation in mind
+  private String title;
+  private String director;
+  private int runningTime;
+  private boolean watched;
+
+  // Define the class's constructor
+  public Movie(String movieTitle, String movieDirector, int movieLengthInMinutes) {
+    title = movieTitle;
+    director = movieDirector;
+    runningTime = movieLengthInMinutes;
+    watched = false;
+  }
+
+  // Include getters and setters for the class's variables
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDirector() {
+    return director;
+  }
+
+  public void setDirector(String director) {
+    this.director = director;
+  }
+
+  public int getRunningTime() {
+    return runningTime;
+  }
+
+  public void setRunningTime(int runningTime) {
+    this.runningTime = runningTime;
+  }
+
+  public boolean isWatched() {
+    return watched;
+  }
+
+  public void setWatched(boolean watched) {
+    this.watched = watched;
+  }
+
+  public void startMovie() {
+    setWatched(true);
+  }
+
+  public String toString() {
+    String movieInfo = "Title: " + title + "\nDirector: " + director + "\nRunning Time: "
+        + runningTime + "\nWatched: " + watched;
+
+    return movieInfo;
+  }
+}
---
+++ chapter4_writing_classes/src/chapter4_writing_classes/WatchIndianaJonesSeries.java
@@ -0,0 +1,27 @@
+package chapter4_writing_classes;
+
+public class WatchIndianaJonesSeries {
+  // An example of a constant
+  private final static String DIRECTOR = "Steven Spielberg";
+
+  public static void main(String[] args) {
+    Movie movie1 = new Movie("Indiana Jones and the Raiders of the Lost Ark", DIRECTOR, 115);
+    watchMovie(movie1);
+
+    Movie movie2 = new Movie("Indiana Jones and the Temple of Doom", DIRECTOR, 118);
+    watchMovie(movie2);
+
+    Movie movie3 = new Movie("Indiana Jones and the Last Crusade", DIRECTOR, 128);
+    watchMovie(movie3);
+
+    Movie movie4 = new Movie("Indiana Jones and the Kingdom of the Crystal Skull", DIRECTOR, 122);
+    watchMovie(movie4);
+  }
+
+  public static void watchMovie(Movie movie) {
+    System.out.println(movie.toString());
+    movie.startMovie();
+    System.out.println("Movie started.\nEating popcorn.\nDid you finish the movie?: "
+        + (movie.isWatched() ? "Yes" : "No") + "\n");
+  }
+}

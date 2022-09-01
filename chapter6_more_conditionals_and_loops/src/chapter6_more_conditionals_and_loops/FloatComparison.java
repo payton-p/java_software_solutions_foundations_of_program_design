6a6ce1a89622639cc61749b0764f8879f3d2212a
Payton Pietron <paytonpietron@gmail.com>
2022-09-01 18:14:43 -0400

    Add float comparison

---
+++ chapter6_more_conditionals_and_loops/src/chapter6_more_conditionals_and_loops/FloatComparison.java
@@ -0,0 +1,16 @@
+package chapter6_more_conditionals_and_loops;
+
+public class FloatComparison {
+  public static void main(String[] args) {
+    double a = 1.398447484;
+    double b = 1.398447482;
+
+    // This allows us to specify how precise the comparison needs to be
+    double precisionLimit = 1e-9;
+
+    // This is an example of how a conditional operator is used
+    String message = Math.abs(a - b) < precisionLimit ? "are" : "are not";
+
+    System.out.println("The numbers " + message + " equal.");
+  }
+}

143c64278296a9f564f987273c6adf79b7da3adf
Payton Pietron <paytonpietron@gmail.com>
2022-09-15 20:36:45 -0400

    Clean up driver class

--- chapter10_polymorphism/src/sorting/LibraryOrganizer.java
+++ chapter10_polymorphism/src/sorting/LibraryOrganizer.java
@@ -3,16 +3,33 @@ package sorting;
 import inheritance_example.Book;
 
 public class LibraryOrganizer {
-  public static void main(String[] args) {
+  private static Book[] createUnsortedLibrary() {
     Book[] library = new Book[3];
 
     library[0] = new Book(100, "Peter and Wendy", "Fiction");
     library[1] = new Book(323, "Frankenstein", "Fiction");
     library[2] = new Book(145, "The Bell Jar", "Fiction");
 
-    Sorting.selectionSort(library);
+    return library;
+  }
 
-    for (Book book : library) {
+  public static void main(String[] args) {
+    // Selection sort testing
+    Book[] library1 = createUnsortedLibrary();
+    Sorting.selectionSort(library1);
+
+    System.out.println("Selection Sort");
+    for (Book book : library1) {
+      System.out.println(book);
+    }
+    System.out.println();
+
+    // Insertion sort testing
+    Book[] library2 = createUnsortedLibrary();
+    Sorting.insertionSort(library2);
+
+    System.out.println("Insertion Sort");
+    for (Book book : library2) {
       System.out.println(book);
     }
   }
